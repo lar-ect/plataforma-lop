f527d1f6b7bdcfdea832a48b90e203dc42592e4a
Gabriel Tibúrcio <tibuurcio@gmail.com>
2017-07-16 06:29:09 -0300

    test(testes): Corrige testes para refletir mudança em api

--- negocio/__tests__/executar.js
+++ negocio/__tests__/executar.js
@@ -11,26 +11,27 @@ const codigoSomatorio = `
 describe("execução de código javascript", () => {
   describe("retorna resultado com mensagem de erro", () => {
     test("erro ao tentar ler entradas em excesso", () => {
-      const resultado = executar(["1", "2"], codigoSomatorio);
+      const resultado = executar(codigoSomatorio, ["1", "2"]);
       expect(resultado).toBe("Erro: Entrada indisponível");
     });
   });
 
   describe("retorna resultado com sucesso", () => {
     test("resultado correto para somatório de 3 entradas", () => {
-      const resultado = executar(["1", "2", "3"], codigoSomatorio);
+      const resultado = executar(codigoSomatorio, ["1", "2", "3"]);
       expect(resultado).toBe("6");
     });
 
     test("resultado correto para escreva com múltiplas linhas", () => {
-      const resultado = executar(["1", "2", "3"], 
-      `
+      const resultado = executar(
+        `
         for(var i = 0; i < 3; i++) {
           escreva(lerInteiro(), true);
         }
-      `
+      `,
+        ["1", "2", "3"]
       );
       expect(resultado).toBe("1\n2\n3\n");
-    })
+    });
   });
 });
