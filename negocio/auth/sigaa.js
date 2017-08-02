800c6d25aa747e202ae5a41de62efc605bff5092
Gabriel Tibúrcio <tibuurcio@gmail.com>
2017-08-01 21:10:20 -0300

    fix(auth sigaa): Correções no modelo de autenticação com o sigaa
    O sistema agora detecta se o usuário é um docente ou um discente, caso seja docente, o sistema
    recupera a lista de turmas do mesmo.

--- negocio/auth/sigaa.js
+++ negocio/auth/sigaa.js
@@ -1,8 +1,8 @@
-const passport = require("passport");
-const OAuth2Strategy = require("passport-oauth2").Strategy;
-const axios = require("axios");
-const mongoose = require("mongoose");
-const User = mongoose.model("User");
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth2').Strategy;
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const SIGAA = require('./sigaa-api');
 
 passport.use(new OAuth2Strategy({
     authorizationURL: 'http://apitestes.info.ufrn.br/authz-server/oauth/authorize',
@@ -12,47 +12,100 @@ passport.use(new OAuth2Strategy({
     callbackURL: "http://localhost:8080/auth/sigaa/callback"
   },
   async (accessToken, refreshToken, sigaaData, data, done) => {
-	const requisicoes = axios.create({
-		baseURL: 'http://apitestes.info.ufrn.br/ensino-services/services/',
-		headers: {
-			'Authorization': `Bearer ${accessToken}`
-		}
-	});
-	try {
-		let req = await requisicoes.get('consulta/listavinculos/usuario');
-		const dadosDiscente = req.data.discentes[0];
-		req = await requisicoes.get(`consulta/discente/${dadosDiscente.matricula}`);
-		const data = req.data;
+		const sigaa = new SIGAA(accessToken);
 
-		let user = await User.findOne({ matricula: dadosDiscente.matricula });
+		try {
+			// Requisita informações do sigaa e identifica se é discente ou docente
+			const listaVinculos = await sigaa.listaVinculos();
+			const dadosVinculo = getDadosVinculo(listaVinculos.data);
 
-		if(!user) {
-			console.log(`Nenhum usuário cadastrado com a matrícula: ${dadosDiscente.matricula}, criando novo usuário...`);
-			user = new User({
-				matricula: dadosDiscente.matricula,
-				nome: data.nome,
-				sigaa: {
-					dadosDiscente,
-					curso: data.curso,
-					matricula: data.matricula,
-					login: data.login
+			if (!dadosVinculo) {
+				console.log(`Usuário sem vínculo tentou se registrar no sistema`);
+				done(new Error('Nenhum vínculo encontrado para o usuário informado'));
+			}
+
+			let dadosSigaa, matricula;
+			if (dadosVinculo.tipo === 'docente') {
+				matricula = dadosVinculo.dados.siape;
+				console.log('Usuário identificado como docente');
+				const docente = dadosVinculo.dados;
+				console.log(JSON.stringify(docente, null, '  '));
+
+				// Recupera a lista de turmas do docente sem os dados dos participantes
+				let listaTurmas = await sigaa.listaTurmasDocente();
+				listaTurmas = listaTurmas.data;
+				console.log(`${listaTurmas.length} turmas recuperadas para o docente`);
+				
+				// Para cada turma de lop, recupera os participantes e junta ao objeto turma
+				for(let i = 0; i < listaTurmas.length; i++) {
+					if (listaTurmas[i].codigoComponente === 'ECT2203') {
+						const turma = listaTurmas[i];
+						console.log(`Recuperando participantes para a turma: ${turma.descricaoCompleta}`);
+						let participantes = await sigaa.listaParticipantesTurma(turma.id);
+						participantes = participantes.data;
+						delete participantes.monitores;
+						turma.participantes = participantes;
+					}
 				}
-			});		
-		} else { // Já existe um usuário com essa matrícula cadastrado
-			console.log(`Usuário encontrado para matrícula: ${dadosDiscente.matricula}, adicionando informações do sigaa`);
-			user.sigaa = {
-				dadosDiscente,
-				curso: data.curso,
-				matricula: data.matricula,
-				login: data.login
-			};
+
+				dadosSigaa = {
+					vinculo: docente,
+					listaTurmas
+				};
+			}
+			else { // Aluno
+				matricula = dadosVinculo.dados.matricula;
+				console.log('Usuário identificado como discente');
+				const discente = dadosVinculo.dados;
+				console.log(JSON.stringify(discente, null, '  '));
+				dadosSigaa = {
+					matricula,
+					vinculo: discente
+				};
+			}
+
+			// Busca por algum usuário no db com a matrícula cadastrada (docente matrícula = siape)
+			let user = await User.findOne({ matricula });
+
+			if(!user) {
+				console.log(`Nenhum usuário cadastrado com a matrícula ou siape: ${matricula}, criando novo usuário...`);
+				user = new User({
+					matricula,
+					sigaa: dadosSigaa
+				});		
+			} else { 
+				console.log(`Usuário encontrado para matrícula: ${matricula}, adicionando informações do sigaa`);
+				user.sigaa = dadosSigaa;
+			}
+			
+			user.markModified("sigaa");
+			user = await user.save();
+			done(null, user);
+		} catch(err) {
+			console.error(err);
+			done(err);
 		}
-		
-		user.markModified("sigaa");
-		user = await user.save();
-		done(null, user);
-	} catch(err) {
-		console.error(err);
-		done(err);
-	}
 }));
+
+function getDadosVinculo(dadosReq) {
+	console.log('Novos dados do sigaa chegando para autenticação.');
+	let dados = dadosReq.docentes[0];
+	let tipo = 'docente';
+
+	if (!dados) {
+		dados = dadosReq.discentes[0];
+		tipo = 'discente';
+		console.log(`Registro de discente. Matrícula: ${dados.matricula}, Curso: ${dados.curso}`);
+	} else {
+		console.log(`Registro de docente. Siape: ${dados.siape}, Lotação: ${dados.lotacao}`);
+	}
+
+	// Se não possuir vínculo
+	if (!dados) {
+		return undefined;
+	}
+
+	return {
+		tipo, dados
+	};
+}
