f14bc2704fbcff5190d3d4872882edf03a261229
Gabriel Tibúrcio <tibuurcio@gmail.com>
2017-08-14 22:52:43 -0300

    style(funções disponíveis): Altera o estilo das funções disponíveis para uma tabela e adiciona promp

--- helpers.js
+++ helpers.js
@@ -17,12 +17,24 @@ exports.icon = name => fs.readFileSync(`./public/images/icons/${name}.svg`);
 // Some details about the site
 exports.siteName = 'LoP';
 
+// exports.funcoesDisponiveis = `
+//   <code>prompt()</code> - Lê uma string da entrada<br>
+//   <code>alert(x)</code> - Escreve <b>x</b> na saída<br>
+//   <code>escreva(x)</code> - Escreve <b>x</b> na saída<br>
+//   <code>lerTexto()</code> - Lê uma string da entrada<br>
+// `;
+
 exports.funcoesDisponiveis = `
-  <code>escreva(x)</code> - Escreve <b>x</b> na saída<br>
-  <code>lerTexto()</code> - Lê uma string da entrada<br>
-  <code>lerInteiro()</code> - Lê um número inteiro da entrada<br>
-  <code>lerReal()</code> - Lê um número real da entrada<br>
-  <code>potencia(b, e)</code> - Retorna <b>b</b> elevado a <b>e</b><br>
-  <code>raizQuadrada(x)</code> - Retorna a raiz quadrada de <b>x</b><br>
-  <code>divisaoInteira(a, b)</code> - Retorna o valor inteiro da divisão de <b>a/b</b>
+  <table class="table table-sm">
+    <thead><tr><th style="text-align: center;">Função</th><th style="text-align: center;">Descrição</th></tr></thead>
+    <tbody>
+      <tr><td><code>prompt()</code></td><td>Lê uma string da entrada</td></tr>
+      <tr><td><code>alert(x)</code></td><td>Escreve <b>x</b> na saída</td></tr>
+      <tr><td><code>escreva(x)</code></td><td>Lê uma string da entrada</td></tr>
+      <tr><td><code>lerInteiro()</code></td><td>Lê um número inteiro da entrada</td></tr>
+      <tr><td><code>lerReal()</code></td><td>Lê um número real da entrada</td></tr>
+      <tr><td><code>potencia(b, e)</code></td><td>Retorna <b>b</b> elevado a <b>e</b></td></tr>
+      <tr><td><code>raizQuadrada(x)</code></td><td>Retorna a raiz quadrada de <b>x</b></td></tr>
+      <tr><td><code>divisaoInteira(a, b)</code></td><td>Retorna o valor inteiro da divisão de <b>a/b</td></tr>
+  </table>
 `;
