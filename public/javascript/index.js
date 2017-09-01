c8f16f3e59b5b96f85cb95c7e05d523f2de50aee
Gabriel Tibúrcio <tibuurcio@gmail.com>
2017-08-31 22:29:24 -0300

    fix(like): Corrige bug que não era mais possível dar like nas questões da página inicial
    Devido a mudança de layout para uma tabela, não era mais possível favoritar questões pela tabela
    devido a linha inteira ter se tornado um link para a página das questões.
    
    fixes #47

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -12,6 +12,14 @@ $(function() {
     });
   });
 
+  /**
+   * Impede a propagação do evento de clique na row da tabela de questões 
+   * quando o usuário clica em um dos botões de like
+   */
+  $('button[name="btn_favoritar"]').click(function(e) {
+    e.stopPropagation();
+  });
+
   const favoritarForms = $('form[name=\'favoritar-questao-form\']');
   favoritarForms.on('submit', function(event) {
     event.preventDefault();
@@ -20,7 +28,10 @@ $(function() {
       .then(res => {
         console.log(`Quantidade de likes: ${res.data.likes}`);
 
-        // Retorna true caso a classe não exista e seja adicionada e false caso já exista e retira
+        /**
+         * Lógica para adicionar ou remover as classes de layout caso a questão
+         * esteja sendo favoritada ou desfavoritada
+         */
         const favoritado = this.btn_favoritar.classList.toggle('favoritado');
         console.log(`Questão foi ${favoritado ? 'favoritada' : 'desfavoritada'}`);
         $(this).find('span[name=\'btn_favoritar_likes\']').text(res.data.likes);
