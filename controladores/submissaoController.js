fd860b7b67f4f51398e5cfeb933eb5e0d9178d2b
orivaldosantana <orivaldo@gmail.com>
2017-09-20 14:37:29 -0300

    corrige permissão de visualização de submissão, fixes #56

--- controladores/submissaoController.js
+++ controladores/submissaoController.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
+const permissoes = require('../dominio/Permissoes');
 const Submissao = mongoose.model('Submissao');
 
 exports.getSubmissao = async (req, res) => {
-	const submissao = await Submissao.findOne({ _id: req.params.id }).populate('user');
-  res.render('submissao/index', { title: `Submissão #${submissao._id}`, submissao });
+  const submissao = await Submissao.findOne({ _id: req.params.id }).populate('user');
+  if ( req.user &&  (req.user.id == submissao.user.id || permissoes.isProfessor(req.user) ) ) {
+    res.render('submissao/index', { title: `Submissão #${submissao._id}`, submissao });
+  }
+  else {
+     req.flash('warning', 'Oops, você não pode acessar essa página');
+     res.redirect('back');
+  }
 };
