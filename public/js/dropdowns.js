f162e52e95df107f2501cd852876e17fb98a4f1f
Clemilson Morais <clemilsonmorais@gmail.com>
2017-09-27 21:45:18 -0300

    feat(layout): Adição de menu com o nome e avatar do usuário
    close #27

---
+++ public/js/dropdowns.js
@@ -0,0 +1,12 @@
+$(function () {
+    var $dropdown = $('.has-dropdown');
+    $dropdown.click(function (event) {
+        $(this).toggleClass('is-active');
+        event.stopPropagation();
+    });
+
+    $(window).click(function () {
+        $dropdown.removeClass('is-active');
+    });
+
+});
