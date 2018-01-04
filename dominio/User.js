e405a277ae8fa4c01cdbf47a4c6888828a638960
Gabriel Tiburcio <tibuurcio@gmail.com>
2018-01-04 10:38:54 -0300

    fix(mongoose): Adequa os models ao mongoose v5
    No mongoose v5, os models precisam definir um nome para a collection pois caso contrário o nome
    padrão é decidido automaticamente

--- dominio/User.js
+++ dominio/User.js
@@ -7,40 +7,43 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 const Permissoes = require('./Permissoes');
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: 'Email já cadastrado no sistema',
-    lowercase: true,
-    trim: true,
-    validate: [validator.isEmail, 'Email inválido'],
-    required: 'Forneça um endereço de email'
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: 'Email já cadastrado no sistema',
+      lowercase: true,
+      trim: true,
+      validate: [validator.isEmail, 'Email inválido'],
+      required: 'Forneça um endereço de email'
+    },
+    matricula: {
+      type: String,
+      unique: 'Matrícula já cadastrada no sistema',
+      trim: true
+    },
+    nome: {
+      type: String,
+      required: 'Forneça um nome',
+      trim: true
+    },
+    grupos: {
+      type: [String],
+      enum: Permissoes.grupos,
+      default: ['USUARIO'],
+      required: true
+    },
+    // githubData: {},
+    sigaa: {
+      turmas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Turma' }]
+    },
+    questoesFavoritas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Questao' }],
+    listasFavoritas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ListaExercicio' }],
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
   },
-  matricula: {
-    type: String,
-    unique: 'Matrícula já cadastrada no sistema',
-    trim: true
-  },
-  nome: {
-    type: String,
-    required: 'Forneça um nome',
-    trim: true
-  },
-  grupos: {
-    type: [String],
-    enum: Permissoes.grupos,
-    default: ['USUARIO'],
-    required: true
-  },
-  // githubData: {},
-  sigaa: {
-    turmas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Turma' }]
-  },
-  questoesFavoritas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Questao' }],
-  listasFavoritas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ListaExercicio' }],
-  resetPasswordToken: String,
-  resetPasswordExpires: Date
-});
+  { collection: 'users' }
+);
 
 userSchema.virtual('gravatar').get(function() {
   const hash = md5(this.email);
